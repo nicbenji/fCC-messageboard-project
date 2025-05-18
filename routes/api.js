d7f3aade21783bac4bf083b0f12f21dcdd2a44c5
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-05-18 22:17:25 +0200

    added empty req methods

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,23 @@
 'use strict';
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+module.exports = function(app) {
+
+    app.route('/api/threads/:board')
+        .post((err, res) => {
+        })
+        .get((err, res) => {
+        })
+        .delete((err, res) => {
+        });
+
+    app.route('/api/replies/:board')
+        .put((err, res) => {
+        })
+        .post((err, res) => {
+        })
+        .get((err, res) => {
+        })
+        .delete((err, res) => {
+        });
 
 };
