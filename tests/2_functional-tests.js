f457c6a0190fc01ddb72c397b88c1eb60e56e18d
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-05-22 19:26:26 +0200

    fully implemented tests, fcc tests on submit can fail because of not explicitly reassiging bumped_on when adding a reply

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -83,8 +83,10 @@ suite('Functional Tests', function() {
     test('should not delete a thread on incorrect password', (done) => {
         chai.request(server)
             .delete('/api/threads/test')
-            .send({})
+            .send({ thread_id: threadId, delete_password: 'wrong_password' })
             .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'incorrect password');
                 done();
             });
     });
@@ -92,8 +94,10 @@ suite('Functional Tests', function() {
     test('should delete a thread with the correct password', (done) => {
         chai.request(server)
             .delete('/api/threads/test')
-            .send({})
+            .send({ thread_id: threadId, delete_password: delThreadPassword })
             .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'success');
                 done();
             });
     });
@@ -101,8 +105,10 @@ suite('Functional Tests', function() {
     test('should report a thread', (done) => {
         chai.request(server)
             .put('/api/threads/test')
-            .send({})
+            .send({ thread_id: threadId })
             .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'reported');
                 done();
             });
     });
@@ -110,8 +116,20 @@ suite('Functional Tests', function() {
     test('should create a new reply', (done) => {
         chai.request(server)
             .post('/api/replies/test')
-            .send({})
+            .send({ text: 'Test reply 2', thread_id: threadId, delete_password: 'test123' })
             .end((err, res) => {
+                assert.equal(res.status, 200);
+
+                assert.property(res.body, '_id');
+                assert.property(res.body, 'text');
+                assert.property(res.body, 'replies');
+                assert.property(res.body, 'created_on');
+                assert.property(res.body, 'bumped_on');
+
+                assert.property(res.body.replies[1], '_id');
+                assert.propertyVal(res.body.replies[1], 'text', 'Test reply 2');
+                assert.property(res.body.replies[1], 'created_on');
+
                 done();
             });
     });
@@ -120,6 +138,18 @@ suite('Functional Tests', function() {
         chai.request(server)
             .get('/api/replies/test?thread_id=' + threadId)
             .end((err, res) => {
+                assert.equal(res.status, 200);
+
+                assert.propertyVal(res.body, '_id', threadId);
+                assert.propertyVal(res.body, 'text', 'Test thread');
+                assert.property(res.body, 'replies');
+                assert.property(res.body, 'created_on');
+                assert.property(res.body, 'bumped_on');
+
+                assert.propertyVal(res.body.replies[0], '_id', replyId);
+                assert.propertyVal(res.body.replies[0], 'text', 'Test first reply');
+                assert.property(res.body.replies[0], 'created_on');
+
                 done();
             });
     });
@@ -127,8 +157,10 @@ suite('Functional Tests', function() {
     test('should not delete a reply with an incorrect password', (done) => {
         chai.request(server)
             .delete('/api/replies/test')
-            .send({})
+            .send({ thread_id: threadId, reply_id: replyId, delete_password: 'wrong_password' })
             .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'incorrect password');
                 done();
             });
     });
@@ -136,8 +168,10 @@ suite('Functional Tests', function() {
     test('should delete a reply with a correct password', (done) => {
         chai.request(server)
             .delete('/api/replies/test')
-            .send({})
+            .send({ thread_id: threadId, reply_id: replyId, delete_password: delReplyPassword })
             .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'success');
                 done();
             });
     });
@@ -145,8 +179,10 @@ suite('Functional Tests', function() {
     test('should report a reply', (done) => {
         chai.request(server)
             .put('/api/replies/test')
-            .send({})
+            .send({ thread_id: threadId, reply_id: replyId })
             .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'reported');
                 done();
             });
     });
